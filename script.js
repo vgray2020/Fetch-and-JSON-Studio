20956862bc40b85a25586bb5b232600aa08340fe
Victoria <weddev2021vg@gmail.com>
2021-04-25 09:16:38 -0500

    commit

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// TODO: add code here
+window.addEventListener("load", function () {
+  fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(
+    function (response) {
+      response.json().then(function (json) {
+
+        const astronaut = document.getElementById("container");
+        
+        let index = 0;
+        
+        astronaut.innerHTML = `
+        <h3>${json[index].firstName} ${json[index].lastName} </h3>
+            <ul>
+            <li>Hours in space: ${json[index].hoursInSpace}</li>
+            <li>Active: ${json[index].active} </li>
+            <li>Skills: ${json[index].skills} </li>
+            </ul>
+            <img src=${json[index].picture} height=150></img>
+          `;
+        index = (index + 1) % json.length;
+      });
+    }
+  );
+});
