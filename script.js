f6e52f2f43298266e3af400b81da2298649df67c
Victoria <weddev2021vg@gmail.com>
2022-03-10 19:37:53 -0600

    finish

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-window.addEventListener("load", function () {
-  fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(
-    function (response) {
-      response.json().then(function (json) {
+window.addEventListener('load', function () {
+  fetch('https://handlers.education.launchcode.org/static/astronauts.json')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      console.log(json);
 
-        const astronaut = document.getElementById("container");
-        
-        let index = 0;
-        
-        astronaut.innerHTML = `
-        <h3>${json[index].firstName} ${json[index].lastName} </h3>
-            <ul>
-            <li>Hours in space: ${json[index].hoursInSpace}</li>
-            <li>Active: ${json[index].active} </li>
-            <li>Skills: ${json[index].skills} </li>
-            </ul>
-            <img src=${json[index].picture} height=150></img>
-          `;
-        index = (index + 1) % json.length;
+      let container = document.getElementById('container');
+      let count = document.getElementById('count');
+      count.innerHTML += `${json.length}`;
+      let astronauts = '';
+      json.sort(function (a, b) {
+        return a.hoursInSpace > b.hoursInSpace ? -1 : 1;
       });
-    }
-  );
+      //use FOR OF loop
+      for (let astronaut of json) {
+        astronauts += `<div class = "astronaut">
+          <div class = "bio">
+            <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+            <ul>
+              <li>Hours in space: ${astronaut.hoursInSpace}</li>
+              <li style = "color:${astronaut.active ? 'green' : 'black'}">Active: ${astronaut.active}</li>
+              <li>Skills: ${astronaut.skills.join(', ')}</li>
+            </ul>
+          </div>
+          <img src ="${astronaut.picture}" alt="" class= "avatar"/>
+        </div>`;
+      }
+
+      container.innerHTML = astronauts;
+    });
 });
+
